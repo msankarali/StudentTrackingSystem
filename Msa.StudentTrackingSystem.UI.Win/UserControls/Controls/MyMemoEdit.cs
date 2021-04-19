b1596c7a30da8138266abbca11d0bc5ca898c719
MUHARREM SERVET ANKARALI <mservetankarali@gmail.com>
2021-04-19 21:26:57 +0300

    memo edit

---
+++ Msa.StudentTrackingSystem.UI.Win/UserControls/Controls/MyMemoEdit.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraEditors;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace Msa.StudentTrackingSystem.UI.Win.UserControls.Controls
+{
+    [ToolboxItem(true)]
+    public class MyMemoEdit : MemoEdit
+    {
+        public MyMemoEdit()
+        {
+            Properties.AppearanceFocused.BackColor = Color.LightCyan;
+            Properties.MaxLength = 500;
+        }
+        public override bool EnterMoveNextControl { get; set; } = true;
+    }
+}
